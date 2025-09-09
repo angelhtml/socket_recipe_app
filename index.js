dae95e2dcb16d07d47df0b146e097e7f6bbe5445
angelhtml <khfarhad6@gmail.com>
2025-09-09 22:41:34 +0330

    socket cors fixed

--- index.js
+++ index.js
@@ -23,8 +23,11 @@ const server = http.createServer(app)
 const io = new Server(server, {
     cors: {
         origin: `${process.env.CLIENT}`,
-        methods: ["GET","POST"]
-    }
+        methods: ["GET","POST"],
+        transports: ['websocket', 'polling'],
+        credentials: true
+    },
+    allowEIO3: true
 })
 
 io.on("connection", (socket) => {
